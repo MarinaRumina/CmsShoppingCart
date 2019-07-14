2779d22352b3cdff2fa119bccd9339128981f187
Marina Rumyansev <aniramrum@gmail.com>
2019-07-14 18:01:24 +0300

    Add PageDTO and PageViewModel

---
+++ CmsShoppingCart/Models/ViewModels/Pages/PageVM.cs
@@ -0,0 +1,35 @@
+﻿using CmsShoppingCart.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CmsShoppingCart.Models.ViewModels.Pages
+{
+    public class PageVM
+    {
+        public PageVM(PageDTO row)
+        {
+            this.Id = row.Id;
+            this.Title = row.Title;
+            this.Slug = row.Slug;
+            this.Body = row.Body;
+            this.Sorting = row.Sorting;
+            this.HasSidebar = row.HasSidebar;
+        }
+
+        public PageVM() { }
+
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 3)]
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        public bool HasSidebar { get; set; }
+    }
+}
