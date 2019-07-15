9083be1ffed9080e56f3909237808111bda72fc4
Marina Rumyansev <aniramrum@gmail.com>
2019-07-15 17:52:57 +0300

    Adding AddPage POST method

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -27,9 +27,65 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
         }
 
         // GET: Admin/Pages/AddPage
+        [HttpGet]
         public ActionResult AddPage()
         {
             return View();
+        }
+
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            // Check model state
+            if (!ModelState.IsValid)
+            {
+                return View(model); // the fields that were filled out will be returned filled out so no need to fill them again
+            }
+
+            using (Db db = new Db())
+            {
+                // Declare slug
+                string slug;
+
+                // Init pageDTO
+                PageDTO dto = new PageDTO();
+
+                // DTO title
+                dto.Title = model.Title;
+
+                // Check for and set slug if have to be
+                if (string.IsNullOrWhiteSpace(model.Slug)) // checking if empty
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+                // Make sure title and slug are unique
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == model.Slug))
+                {
+                    ModelState.AddModelError("", "The title or slug already exists");
+                    return View(model);
+                }
+
+                // DTO the rest
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100; // the page that we adding will always be the last one
+
+                // Save DTO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+
+            }
+            // Set TempData message (like ViewBag, just allows to save and transmit the data after redirection (unlike ViewBag))
+            TempData["SuccessMessage"] = "You've added a new page!";
+
+            // Redirect
+            return RedirectToAction("AddPage"); // redirects to AddPage GET method 
         }
     }
 }
