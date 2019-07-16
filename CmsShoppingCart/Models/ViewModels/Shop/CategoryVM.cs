b20df533234ebc0a22b5414a93fb73d579f13493
Marina Rumyansev <aniramrum@gmail.com>
2019-07-16 17:57:57 +0300

    Adding Category ViewModel

---
+++ CmsShoppingCart/Models/ViewModels/Shop/CategoryVM.cs
@@ -0,0 +1,29 @@
+﻿using CmsShoppingCart.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CmsShoppingCart.Models.ViewModels.Shop
+{
+    public class CategoryVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public int Sorting { get; set; }
+
+        public CategoryVM(CategoryDTO row)
+        {
+            this.Id = row.Id;
+            this.Name = row.Name;
+            this.Slug = row.Slug;
+            this.Sorting = row.Sorting;
+        }
+
+        public CategoryVM()
+        {
+
+        }
+    }
+}
