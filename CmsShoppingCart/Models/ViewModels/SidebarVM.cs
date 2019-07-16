c5da7d9ba54b7da07ee6cb92a7f88268e267c62a
Marina Rumyansev <aniramrum@gmail.com>
2019-07-16 13:58:19 +0300

    Adding Sidebar table/DTO/viewmodel

---
+++ CmsShoppingCart/Models/ViewModels/SidebarVM.cs
@@ -0,0 +1,24 @@
+﻿using CmsShoppingCart.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CmsShoppingCart.Models.ViewModels
+{
+    public class SidebarVM
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+
+        public SidebarVM(SidebarDTO row)
+        {
+            this.Id = row.Id;
+            this.Body = row.Body;
+        }
+        public SidebarVM()
+        {
+
+        }
+    }
+}
