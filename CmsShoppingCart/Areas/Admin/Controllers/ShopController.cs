fa236b9ad8773ba4f7970db29ba6e8812f8ee2b9
Marina Rumyantsev <aniramrum@gmail.com>
2019-07-17 00:26:39 +0300

    Adding Add new Category functionality

--- CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
@@ -29,5 +29,41 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             // Return the view with the list
             return View(categoryVMList);
         }
+
+        // POST: Admin/Shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory (string catName)
+        {
+            // Declare an id
+            string id;
+
+            using (Db db = new Db())
+            {
+                // Check that the category name is unique
+                if (db.Categories.Any(x => x.Name == catName))
+                {
+                    return "The category with such name is already exists.";
+                }
+
+                // Init DTO
+                CategoryDTO dto = new CategoryDTO();
+
+                // Add to DTO
+                dto.Name = catName;
+                dto.Slug = catName.Replace(' ', '-').ToLower();
+                dto.Sorting = 100;
+
+                // Save DTO
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                // Get the id
+                id = dto.Id.ToString();
+
+            }
+            // Return id
+
+            return id;
+        }
     }
 }
