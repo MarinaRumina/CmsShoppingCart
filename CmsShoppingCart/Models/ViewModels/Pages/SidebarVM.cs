e358f340cefd08ea995ca27ff6cabf60f6d643bd
Marina Rumyansev <aniramrum@gmail.com>
2019-07-16 17:01:38 +0300

    Adding WYSIWYG Editor to the EditSidebar page, adding AllowHtml annotation to the SidebarVM

--- CmsShoppingCart/Models/ViewModels/Pages/SidebarVM.cs
+++ CmsShoppingCart/Models/ViewModels/Pages/SidebarVM.cs
@@ -3,12 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace CmsShoppingCart.Models.ViewModels
 {
     public class SidebarVM
     {
         public int Id { get; set; }
+        [AllowHtml]
         public string Body { get; set; }
 
         public SidebarVM(SidebarDTO row)
