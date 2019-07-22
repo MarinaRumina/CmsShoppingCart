52ab634bdc570af0315d8b925902ba8cac785cf6
Marina Rumyansev <aniramrum@gmail.com>
2019-07-22 18:09:37 +0300

    Adding Product ViewModel

---
+++ CmsShoppingCart/Models/ViewModels/Shop/ProductVM.cs
@@ -0,0 +1,46 @@
+﻿using CmsShoppingCart.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Models.ViewModels.Shop
+{
+    public class ProductVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string CategoryName { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        public string ImageName { get; set; }
+
+        // Adding more properties for a viewmodel
+        public IEnumerable<SelectListItem> Categories { get; set; } // to make choice of product's category
+        public IEnumerable<string> GalleryImages { get; set; }
+
+
+        public ProductVM(ProductDTO row)
+        {
+            Id = row.Id;
+            Name = row.Name;
+            Slug = row.Slug;
+            Description = row.Description;
+            Price = row.Price;
+            CategoryName = row.CategoryName;
+            CategoryId = row.CategoryId;
+            ImageName = row.ImageName;
+        }
+        public ProductVM()
+        {
+
+        }
+    }
+}
